9140a62b3060adb982d55171a6b5f345a7db3b89
Daorse <mmorpgplays5@gmail.com>
2025-02-21 16:31:07 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="bg-white w-screen h-screen">
       <h1 className="text-5xl font-black text-black">Homepage</h1>
+      <p>Test</p>
     </main>
   );
 }
